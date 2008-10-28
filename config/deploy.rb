7e2822a639ae3969bc74d52f023f302987ef0e07
pedro@joyent.com <pedro@joyent.com@7a7ed7af-43cf-e7d3-831c-e7cc5cc1171a>
2008-10-28 15:39:53 +0000

    Some updates to deploy file: new accelerator paths & some tasks docs

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,19 +14,28 @@ $Id$
 require 'erb'
 require 'config/accelerator/accelerator_tasks'
 require 'config/recipes/joyent'
- 
+
+set :svn, "/opt/local/bin/svn"
+set :ggrep, "/opt/local/bin/ggrep"
+set :rake, "/opt/local/bin/rake"
+set :sudo, "/opt/local/bin/sudo"
+
 set :application, "connector"
 set :repository,  "http://svn.joyent.com/opensource/connector/source/trunk"
- 
+
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/opt/joyent/#{application}" # I like this location
+set :deploy_via, :remote_cache
  
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :subversion
-set :sudo, "/opt/csw/bin/sudo"
+
+set :use_sudo, false
+set :user, 'joyent'
+set :runner, 'joyent'
 
 # Do not run the sudo command through sh
 # default_run_options[:shell] = false
@@ -50,22 +59,38 @@ depend :remote, :gem, :ezcrypto, '>=0.7.0', :roles => :app
 depend :remote, :gem, :gettext, '=1.9.0', :roles => :app
 depend :remote, :gem, :mongrel_cluster, '>=1.0.4', :roles => :app
  
+desc "Overriden deploy:restart task. It will run
+\naccelerator.smf_restart
+accelerator.restart_apache\n"
 deploy.task :restart do
   accelerator.smf_restart
   accelerator.restart_apache
 end
- 
+
+desc "Overriden deploy:start task. It will run
+\naccelerator.smf_start
+accelerator.restart_apache\n"
 deploy.task :start do
   accelerator.smf_start
   accelerator.restart_apache
 end
- 
+
+desc "Overriden deploy:stop task. It will run
+\naccelerator.smf_stop
+accelerator.restart_apache\n"
 deploy.task :stop do
   accelerator.smf_stop
   accelerator.restart_apache
 end
 
 # Override the default deploy:cold to include the schema load
+desc "Override the default deploy:cold to include schema load before migrate
+
+Deploys and starts a `cold' application. This is useful if you have not deployed
+your application before, or if your application is (for some other reason) not
+currently running. It will deploy the code, run any pending migrations, and then
+instead of invoking `deploy:restart', it will invoke `deploy:start' to fire up
+the application servers."
 deploy.task :cold do
   update
   run "cd #{current_path}; rake RAILS_ENV=production db:schema:load"
